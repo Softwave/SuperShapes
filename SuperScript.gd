e4d80a9d2d63f89229aeec9ed3df5eccfbf426f5
Jessica Leyba <development@softwavestudios.com>
2020-12-04 10:24:13 -0700

    Another update

--- SuperScript.gd
+++ SuperScript.gd
@@ -9,7 +9,21 @@ var glow_bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	get_node("../../../CanvasLayer/WindowDialog/VBoxContainer/M1Slider").value = 8.0
+	get_node("../../../CanvasLayer/WindowDialog/VBoxContainer/N11Slider").value = 60.0
+	get_node("../../../CanvasLayer/WindowDialog/VBoxContainer/N12Slider").value = 100.0
+	get_node("../../../CanvasLayer/WindowDialog/VBoxContainer/N13Slider").value = 30.0
+	
+	get_node("../../../CanvasLayer/WindowDialog/VBoxContainer/A1Slider").value = 1.0
+	get_node("../../../CanvasLayer/WindowDialog/VBoxContainer/B1Slider").value = 1.0
+	
+	get_node("../../../CanvasLayer/WindowDialog/VBoxContainer/M2Slider").value = 2.0
+	get_node("../../../CanvasLayer/WindowDialog/VBoxContainer/N21Slider").value = 10.0
+	get_node("../../../CanvasLayer/WindowDialog/VBoxContainer/N22Slider").value = 10.0
+	get_node("../../../CanvasLayer/WindowDialog/VBoxContainer/N23Slider").value = 10.0
+	
+	get_node("../../../CanvasLayer/WindowDialog/VBoxContainer/A2Slider").value = 1.0
+	get_node("../../../CanvasLayer/WindowDialog/VBoxContainer/B2Slider").value = 1.0
 
 func get_input(delta):
 	pass
@@ -32,26 +46,36 @@ func _on_M1Slider_value_changed(value):
 func _on_N11Slider_value_changed(value):
 	var superMesh = get_node("../MeshInstance")
 	superMesh.get_surface_material(0).set_shader_param("_N11", value)
+	var tempLabel = get_node("../../../CanvasLayer/WindowDialog/VBoxContainer/N1Label")
+	tempLabel.text = "N1 1: " + str(value)
 
 
 func _on_N12Slider_value_changed(value):
 	var superMesh = get_node("../MeshInstance")
 	superMesh.get_surface_material(0).set_shader_param("_N12", value)
+	var tempLabel = get_node("../../../CanvasLayer/WindowDialog/VBoxContainer/N1Label2")
+	tempLabel.text = "N 1 2: " + str(value)
 
 
 func _on_N13Slider_value_changed(value):
 	var superMesh = get_node("../MeshInstance")
 	superMesh.get_surface_material(0).set_shader_param("_N13", value)
+	var tempLabel = get_node("../../../CanvasLayer/WindowDialog/VBoxContainer/N1Label3")
+	tempLabel.text = "N 1 3: " + str(value)
 
 
 func _on_A1Slider_value_changed(value):
 	var superMesh = get_node("../MeshInstance")
 	superMesh.get_surface_material(0).set_shader_param("_A1", value)
+	var tempLabel = get_node("../../../CanvasLayer/WindowDialog/VBoxContainer/A1Label")
+	tempLabel.text = "A1: " + str(value)
 
 
 func _on_B1Slider_value_changed(value):
 	var superMesh = get_node("../MeshInstance")
 	superMesh.get_surface_material(0).set_shader_param("_B1", value)
+	var tempLabel = get_node("../../../CanvasLayer/WindowDialog/VBoxContainer/B1Label")
+	tempLabel.text = "B1: " + str(value)
 
 
 func _on_ButtonToggleControls_pressed():
@@ -61,31 +85,43 @@ func _on_ButtonToggleControls_pressed():
 func _on_M2Slider_value_changed(value):
 	var superMesh = get_node("../MeshInstance")
 	superMesh.get_surface_material(0).set_shader_param("_M2", value)
+	var tempLabel = get_node("../../../CanvasLayer/WindowDialog/VBoxContainer/M2Label")
+	tempLabel.text = "M2: " + str(value)
 
 
 func _on_N21Slider_value_changed(value):
 	var superMesh = get_node("../MeshInstance")
 	superMesh.get_surface_material(0).set_shader_param("_N21", value)
+	var tempLabel = get_node("../../../CanvasLayer/WindowDialog/VBoxContainer/N21Label")
+	tempLabel.text = "N 2 1: " + str(value)
 
 
 func _on_N22Slider_value_changed(value):
 	var superMesh = get_node("../MeshInstance")
 	superMesh.get_surface_material(0).set_shader_param("_N22", value)
+	var tempLabel = get_node("../../../CanvasLayer/WindowDialog/VBoxContainer/N22Label")
+	tempLabel.text = "N 2 2: " + str(value)
 
 
 func _on_N23Slider_value_changed(value):
 	var superMesh = get_node("../MeshInstance")
 	superMesh.get_surface_material(0).set_shader_param("_N23", value)
+	var tempLabel = get_node("../../../CanvasLayer/WindowDialog/VBoxContainer/N23Label")
+	tempLabel.text = "N 2 3: " + str(value)
 
 
 func _on_A2Slider_value_changed(value):
 	var superMesh = get_node("../MeshInstance")
 	superMesh.get_surface_material(0).set_shader_param("_A2", value)
+	var tempLabel = get_node("../../../CanvasLayer/WindowDialog/VBoxContainer/A2Label")
+	tempLabel.text = "A 2: " + str(value)
 
 
 func _on_B2Slider_value_changed(value):
 	var superMesh = get_node("../MeshInstance")
 	superMesh.get_surface_material(0).set_shader_param("_B2", value)
+	var tempLabel = get_node("../../../CanvasLayer/WindowDialog/VBoxContainer/B2Label")
+	tempLabel.text = "B2: " + str(value)
 
 
 func _on_BtnReset_pressed():
